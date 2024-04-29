33b8413783749bd9c1f33ea6929777f2748c957e
Veakor <135655247+Veakor@users.noreply.github.com>
2024-04-29 21:45:50 +0200

    add

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
 import { changeFilter } from "../../redux/filters/slice";
 import { filtersContacts } from "../../redux/filters/selectors";
+
 import clsx from "clsx";
 import css from "./SearchBox.module.css";
 
@@ -11,67 +12,16 @@ const SearchBox = () => {
   const onChangeFilter = (event) => {
     dispatch(changeFilter(event.target.value));
   };
-
   return (
     <div>
+      <h2 className={clsx(css.titleFilter)}>Find contacts by name</h2>
       <input
         className={clsx(css.inputFilter)}
         type="text"
         value={selectNameFilter}
         onChange={onChangeFilter}
-        placeholder="Search contacts" 
       />
     </div>
   );
 };
-
 export default SearchBox;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-{/*import styles from './SearchBox.module.css'; 
-import { useSelector, useDispatch } from 'react-redux';
-import { selectNameFilter, changeFilter } from './redux/filtersSlice';
-
-const SearchBox = () => {
-  const nameFilter = useSelector(selectNameFilter);
-  const dispatch = useDispatch();
-
-  const handleSearchChange = (e) => {
-    dispatch(changeFilter(e.target.value));
-  };
-    return (
-        <div className={styles.searchBoxContainer}>
-      <input
-        className={styles.searchBoxInput}
-        type="text"
-        placeholder="Search contacts..."
-        value={searchTerm}
-        onChange={onSearchChange}
-      />
-    </div>
-    );
-  };
-  
-export default SearchBox;*/}
-  
-  

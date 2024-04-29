b34510e5a139c2303b72703513d8e43df129069f
Veakor <135655247+Veakor@users.noreply.github.com>
2024-04-29 21:42:10 +0200

    add

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
-import { changeFilter } from "../../redux/filtersSlice";
+import { changeFilter } from "../../redux/filters/slice";
 import { filtersContacts } from "../../redux/filters/selectors";
 import clsx from "clsx";
 import css from "./SearchBox.module.css";
